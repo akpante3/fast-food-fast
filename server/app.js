080376c8d3271f0b0fa53302996e3f7ed06970d0
akpante3 <konigeobije@gmail.com>
2018-09-19 13:34:16 +0100

    feature [ #160539560 ] update develop branch

---
+++ server/app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import ordersRoutes from './app/routes/orders';
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/api/v1', ordersRoutes);
+
+const date = new Date();
+app.listen(port, () => {
+  console.log(`running on ${port}  ${date}`);
+});
+
+export default app;
+ 
+ 
---
+++ server/app/db/dbconnect.js
@@ -0,0 +1,29 @@
+const Orders = [];
+
+const menu = [
+  {
+    food: 'fried chicken',
+    foodId: 1,
+  },
+  {
+    food: 'burger',
+    foodId: 2,
+  },
+  {
+    food: 'meat pie',
+    foodId: 3,
+  },
+  {
+    food: 'pizza',
+    foodId: 4,
+  },
+  {
+    food: 'roasted chicken',
+    foodId: 5,
+  },
+];
+
+export {
+  Orders,
+  menu,
+};
