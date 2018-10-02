3e538153c77394ca34c955535a4e0a69ba0277c5
akpante3 <konigeobije@gmail.com>
2018-10-02 02:00:27 +0100

    feature [#160912194 ] post food

--- UI/js/usersAuth.js
+++ UI/js/usersAuth.js
@@ -42,10 +42,7 @@ $(document).ready(() => {
             window.localStorage.setItem(Access_Key, token);
         }
 
-        const validateEmail = (email) => {
-            const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            return re.test(email);
-          };    
+   
     
         $('button.signupbtn').click((e) => {
             e.preventDefault();
